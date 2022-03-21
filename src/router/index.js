3c1e0febaaeebb7f1b826101621140b8932848ad
codeicu <zinkchen@126.com>
2022-03-21 21:50:42 +0800

    增加路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,27 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Login from "../components/Login.vue";
-
+import Home from "../components/Home.vue";
 Vue.use(VueRouter);
 
 const routes = [
   { path: "/login", component: Login },
   { path: "/", redirect: "/login" },
+  { path: "/home", component: Home },
 ];
 
 const router = new VueRouter({
   routes,
+});
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  if (to.path === "/login") return next();
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem("token");
+
+  if (!tokenStr) return next("/login");
+  next();
 });
 
 export default router;
