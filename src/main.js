182ae2bbe6cdea5f6e45168d5c965ac6e9b38f38
codeicu <zinkchen@126.com>
2022-03-27 12:23:00 +0800

    nprogress

--- src/main.js
+++ src/main.js
@@ -7,12 +7,23 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
 
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/'
 axios.interceptors.request.use((config) => {
+  NProgress.start()
   console.log(config)
   config.headers.Authorization = window.sessionStorage.getItem('token')
   return config
 })
+
+axios.interceptors.response.use((config) => {
+  NProgress.done()
+  console.log(config)
+  return config
+})
+
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
